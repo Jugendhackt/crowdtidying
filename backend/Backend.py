67b42f1a16e4fdcaffe7bb4a85d2d2dc593a0df2
rasmus <rasmu>
2020-11-07 16:54:10 +0100

    flask.py hinzugefügt

--- backend/Backend.py
+++ backend/Backend.py
@@ -1,1 +1,20 @@
 #hier kommt das backend hin
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+        host="0.tcp.ngrok.io",
+        port=19398,
+        user="crowdtidying",
+        password="helloWorld!2",
+        database="crowdtidying",
+    )
+
+mycursor = mydb.cursor()
+
+mycursor.execute('SELECT* FROM teams')
+myresults = mycursor.fetchall()
+
+
+for i in myresults:
+    print(i)
