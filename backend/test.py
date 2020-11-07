233384ea85475ead05c9003f5583326db3cd08a1
Jens Hausdorf <mail@jens-hausdorf.de>
2020-11-07 17:37:00 +0100

    Fix typo and format

--- backend/test.py
+++ backend/test.py
@@ -2,23 +2,24 @@ from flask import Flask
 import mysql.connector
 
 
-app = Flask('Hello_World_Test') # name for the Flask app (refer to output)
+app = Flask('Hello_World_Test')  # name for the Flask app (refer to output)
 # running the server
 
 
 mydb = mysql.connector.connect(
-        host="0.tcp.ngrok.io",
-        port=19398,
-        user="crowdtidying",
-        password="helloWorld!2",
-        database="crowdtidying",
-    )
+    host="0.tcp.ngrok.io",
+    port=19398,
+    user="crowdtidying",
+    password="helloWorld!2",
+    database="crowdtidying",
+)
 
-@app.route('/', methods=['GET', 'POST', 'PUT']) # decorator
+
+@app.route('/', methods=['GET', 'POST', 'PUT'])  # decorator
 def home():
     mycursor = mydb.cursor()
 
-    mycursor.execute('SELECT* FROM teams')
+    mycursor.execute('SELECT * FROM teams')
     myresults = mycursor.fetchall()
     results = ''
     for i in myresults:
@@ -27,4 +28,5 @@ def home():
     # route handler function
     # returning a response
 
-app.run(debug = True) # to allow for debugging and auto-reload
+
+app.run(debug=True)  # to allow for debugging and auto-reload
